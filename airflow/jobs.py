718954e2a9b7ab2b35941d5b90a2c8e5fea74a92
Chris Fei <chris@indicative.com>
2017-04-28 19:07:29 -0400

    pool

--- airflow/jobs.py
+++ airflow/jobs.py
@@ -1866,7 +1866,8 @@ class BackfillJob(BaseJob):
                         open_slots = pools[pool].open_slots(session=session)
 
                     if open_slots <= 0:
-                        logging.info("Pool full! Not scheduling task in pool %s".format(pool))
+                        logging.info("Pool full! Not scheduling task in pool %s", pool)
+                        continue
 
                     task = self.dag.get_task(ti.task_id)
                     ti.task = task
