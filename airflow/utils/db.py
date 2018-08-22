1bfe1f5a4edb582ce67e8c61dfc7319b1f6e9aef
Gerardo Curiel <gerardo@gerar.do>
2018-08-22 18:26:54 +1000

    Dockerise CI pipeline (#3393)

--- airflow/utils/db.py
+++ airflow/utils/db.py
@@ -95,12 +95,12 @@ def initdb(rbac=False):
     merge_conn(
         models.Connection(
             conn_id='airflow_db', conn_type='mysql',
-            host='localhost', login='root', password='',
+            host='mysql', login='root', password='',
             schema='airflow'))
     merge_conn(
         models.Connection(
             conn_id='airflow_ci', conn_type='mysql',
-            host='localhost', login='root', extra="{\"local_infile\": true}",
+            host='mysql', login='root', extra="{\"local_infile\": true}",
             schema='airflow_ci'))
     merge_conn(
         models.Connection(
@@ -142,18 +142,19 @@ def initdb(rbac=False):
     merge_conn(
         models.Connection(
             conn_id='mongo_default', conn_type='mongo',
-            host='localhost', port=27017))
+            host='mongo', port=27017))
     merge_conn(
         models.Connection(
             conn_id='mysql_default', conn_type='mysql',
             login='root',
-            host='localhost'))
+            host='mysql'))
     merge_conn(
         models.Connection(
             conn_id='postgres_default', conn_type='postgres',
             login='postgres',
+            password='airflow',
             schema='airflow',
-            host='localhost'))
+            host='postgres'))
     merge_conn(
         models.Connection(
             conn_id='sqlite_default', conn_type='sqlite',
@@ -185,7 +186,7 @@ def initdb(rbac=False):
     merge_conn(
         models.Connection(
             conn_id='sftp_default', conn_type='sftp',
-            host='localhost', port=22, login='travis',
+            host='localhost', port=22, login='airflow',
             extra='''
                 {"key_file": "~/.ssh/id_rsa", "no_host_key_check": true}
             '''))
@@ -288,7 +289,7 @@ def initdb(rbac=False):
     merge_conn(
         models.Connection(
             conn_id='cassandra_default', conn_type='cassandra',
-            host='localhost', port=9042))
+            host='cassandra', port=9042))
 
     # Known event types
     KET = models.KnownEventType
