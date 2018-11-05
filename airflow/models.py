ab1501d4ef5fdaf5dce891a09af461709ed4cff5
Ethan Vizitei <ethan.vizitei@gmail.com>
2018-11-05 11:24:55 -0600

    Call success_callback before updating task state (#4082)
    In cases where the success callback takes variable
    time, it's possible for it to interrupted by the heartbeat process.
    This is because the heartbeat process looks for tasks that are no
    longer in the "running" state but are still executing and reaps them.
    
    This commit reverses the order of callback invocation and state
    updating so that the "SUCCESS" state for the task isn't committed
    to the database until after the success callback has finished.

--- airflow/models.py
+++ airflow/models.py
@@ -1728,14 +1728,6 @@ class TaskInstance(Base, LoggingMixin):
             self.handle_failure(e, test_mode, context)
             raise
 
-        # Recording SUCCESS
-        self.end_date = timezone.utcnow()
-        self.set_duration()
-        if not test_mode:
-            session.add(Log(self.state, self))
-            session.merge(self)
-        session.commit()
-
         # Success callback
         try:
             if task.on_success_callback:
@@ -1744,6 +1736,12 @@ class TaskInstance(Base, LoggingMixin):
             self.log.error("Failed when executing success callback")
             self.log.exception(e3)
 
+        # Recording SUCCESS
+        self.end_date = timezone.utcnow()
+        self.set_duration()
+        if not test_mode:
+            session.add(Log(self.state, self))
+            session.merge(self)
         session.commit()
 
     @provide_session
